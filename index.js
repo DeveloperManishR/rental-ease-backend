0dcfe3752b2ad54be3c4eef6aae9700b027d278b
DeveloperManishR <manishrawat0711@gmail.com>
2026-03-08 19:13:33 +0530

    small change in index file

--- index.js
+++ index.js
@@ -6,29 +6,27 @@ import cookieParser from "cookie-parser";
 import { ConnectDB } from "./config/config.js";
 import apiRoutes from "./routes/index.js";
 
-ConnectDB();
-
 const app = express();
+
+// Connect Database
+ConnectDB();
 
 // Middleware
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
 
-// Serve public folder (uploaded files)
-const __dirname = path.resolve();
-app.use("/public", express.static(path.join(__dirname, "public")));
-
 // CORS
 app.use(
   cors({
-    origin: (origin, cb) =>
-      !origin || ["http://localhost:5173", "http://localhost:3000"].includes(origin)
-        ? cb(null, true)
-        : cb(new Error("Not allowed")),
+    origin: process.env.CLIENT_URL,
     credentials: true,
   })
 );
+
+// Serve public folder (uploaded files)
+const __dirname = path.resolve();
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 // Routes
 app.use("/api", apiRoutes);
@@ -39,6 +37,7 @@ app.get("/", (req, res) => {
 
 // Start Server
 const PORT = process.env.PORT || 4000;
+
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
